5fb558761e49a7504fd73b27f59de0a8756fb9a8
Simgoo <vineethpateel@gmail.com>
2025-04-26 01:48:21 -0400

    fix

--- server/build.sh
+++ server/build.sh
@@ -2,4 +2,4 @@ pip3 install -r requirements.txt
 pip3 install gunicorn
 python3 manage.py makemigrations
 python3 manage.py migrate
-python manage.py runserver 0.0.0.0:$PORT
+gunicorn mysite.wsgi:application --bind 0.0.0.0:$PORT
