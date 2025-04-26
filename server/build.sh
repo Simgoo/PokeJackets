1bf33a489bcac861cacbf620eab17eb78fced3e2
Simgoo <vineethpateel@gmail.com>
2025-04-26 01:50:20 -0400

    fixes

--- server/build.sh
+++ server/build.sh
@@ -1,5 +1,6 @@
 pip3 install -r requirements.txt 
 pip3 install gunicorn
+pip3 install gevent
 python3 manage.py makemigrations
 python3 manage.py migrate
-gunicorn mysite.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn mysite.wsgi:application --bind 0.0.0.0:$PORT --worker-class gevent
